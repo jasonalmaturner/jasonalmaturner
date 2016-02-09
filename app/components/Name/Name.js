09505a84b9d5eaec13e110ecb8c9bf9f1f2908c2
Jason Turner <jasonalmaturner@gmail.com>
2016-02-08 19:22:58 -0700

    changing file structure to implement redux

--- app/components/Name/Name.js
+++ app/components/Name/Name.js
@@ -4,8 +4,8 @@ import styles from './Name.css';
 const Name = () => (
   <div className={styles.contentContainer}>
     <div className={styles.contents}>
-      <img className={styles.profilePic} src='images/profile.jpg' />
-      <h1>Jason Turner</h1>
+      <img className={styles.profilePic} src='images/profile_center.jpg' />
+      <h1 className={styles.myName}>Jason Turner</h1>
     </div>
   </div>
 );
