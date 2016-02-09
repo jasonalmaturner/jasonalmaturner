f0cdc643cb2602f464f28139b4ab70ecb5d8273f
Jason Turner <jasonalmaturner@gmail.com>
2016-02-08 22:24:52 -0700

    creating more actions

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -2,10 +2,28 @@ import axios from 'axios';
 
 export const REQUEST_EVENTS = 'REQUEST_EVENTS';
 export const RECEIVE_EVENTS = 'RECEIVE_EVENTS';
+export const SELECT_USER = 'SELECT_USER';
+export const INVALIDATE_USER = 'INVALIDATE_USER';
 
-function requestEvents(user = 'jasonalmaturner') {
+export function selectUser(user = 'jasonalmaturner') {
+  return {
+    type: SELECT_USER,
+    user,
+  };
+}
+
+export function requestEvents(user = 'jasonalmaturner') {
   return {
     type: REQUEST_EVENTS,
     user,
+  };
+}
+
+export function receiveEvents(user, json) {
+  return {
+    type: RECEIVE_EVENTS,
+    user,
+    posts: json.data.children.map(child => child.data),
+    receivedAt: Date.now(),
   };
 }
