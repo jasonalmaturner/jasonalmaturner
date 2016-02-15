1a15ef9df3543426cda2d1d862012a8be40f6ac5
Jason Turner <jasonalmaturner@gmail.com>
2016-02-14 23:45:58 -0700

    parsing data

--- server-assets/controllers/gitCtrl.js
+++ server-assets/controllers/gitCtrl.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default {
   getEvents(req, res) {
     let user = req.query.user || 'jasonalmaturner';
-    axios.get(`https://api.github.com/users/${user}/events`).then(events => res.json(events)).catch(err => res.status(500).json(err));
+    axios.get(`https://api.github.com/users/${user}/events`).then(events => res.json(events.data)).catch(err => res.status(500).json(err));
   },
 };
