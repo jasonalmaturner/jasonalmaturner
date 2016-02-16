2cf7d30fd91c2ea7d9827f5e4520442c788a38e0
Jason Turner <jasonalmaturner@gmail.com>
2016-02-15 21:14:57 -0700

    working on container

--- app/containers/GitEvents/GitEvents.js
+++ app/containers/GitEvents/GitEvents.js
@@ -1,2 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { selectUser, fetchEventsIfNeeded, invalidateUser } from '../../actions/actions';
+
+class GitEvents extends Component {
+  construct(props) {
+    super(props);
+  }
+}
