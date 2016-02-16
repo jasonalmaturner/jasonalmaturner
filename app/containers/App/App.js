ef83a0f89ae421d63f10fa1ce314624bb104ffa4
Jason Turner <jasonalmaturner@gmail.com>
2016-02-15 20:27:02 -0700

    creating containers

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '../../store/configureStore';
 import styles from './App.css';
@@ -6,12 +6,12 @@ import Name from '../../components/Name/Name';
 
 const store = configureStore();
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className={styles.mainContainer}>
-        <Name />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <div className={styles.mainContainer}>
+      <Name />
+    </div>
+  </Provider>
+);
+
+export default App;
