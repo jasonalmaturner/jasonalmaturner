b818738761b8fdd6ebc12ccfb238ac89ed7b4f85
Jason Turner <jasonalmaturner@gmail.com>
2016-02-18 15:18:30 -0700

    breaking DisplayEvents into child components

---
+++ app/components/DisplayEvent/DisplayEvent.js
@@ -0,0 +1,40 @@
+// I'm breaking this down into a child component, because eventually I
+// want to display all Git Events, not nust the commits from a
+// push event. I think breaking it down into smaller components will
+// make it easier later.
+
+import React, { PropTypes } from 'react';
+import styles from './DisplayEvent.css';
+
+function isPush(event, repo) {
+  if (!event.payload.commits) {
+    // This is kind of uncecessary, but oh well.
+    return <pre><code>{JSON.stringify(event.payload, null, 1)}</code></pre>;
+  }
+
+  return event.payload.commits.map(commit => (
+    <ul key={commit.sha}>
+      {commit.author.name}
+      <p>
+        <a target='_blank' href={`https://github.com/${repo}/commit/${commit.sha}`}>{commit.message}</a>
+      </p>
+    </ul>
+  ));
+}
+
+const DisplayEvent = ({ event }) => (
+  <div>
+    <a target='_blank' href={`https://github.com/${event.repo.name}`}>
+      {event.repo.name}
+    </a>
+    <div>
+      {isPush(event, event.repo.name)}
+    </div>
+  </div>
+);
+
+DisplayEvent.propTypes = {
+  event: PropTypes.object.isRequired,
+};
+
+export default DisplayEvent;
--- app/components/DisplayEvents/DisplayEvents.js
+++ app/components/DisplayEvents/DisplayEvents.js
@@ -1,23 +1,10 @@
 import React, { PropTypes } from 'react';
 import styles from './DisplayEvents.css';
+import DisplayEvent from '../DisplayEvent/DisplayEvent';
 
 // The server currently returns only PushEvents, but isPush is set up
 // in case in the future I want to display other Git events other than
 // just commits.
-function isPush(event, repo) {
-  if (!event.payload.commits) {
-    return JSON.stringify(event.payload);
-  }
-
-  return event.payload.commits.map(commit => (
-    <ul key={commit.sha}>
-      {commit.author.name}
-      <p>
-        <a target='_blank' href={`https://github.com/${repo}/commit/${commit.sha}`}>{commit.message}</a>
-      </p>
-    </ul>
-  ));
-}
 
 const DisplayEvents = ({ theEvents }) => {
   if (!theEvents[0]) {
@@ -25,15 +12,9 @@ const DisplayEvents = ({ theEvents }) => {
   }
 
   const mappedEvents = theEvents.map((theEvent, i) =>
-    <ul key={theEvent.id}>
-      <a target='_blank' href={`https://github.com/${theEvent.repo.name}`}>
-        {theEvent.repo.name}
-      </a>
-      <div>
-        {isPush(theEvent, theEvent.repo.name)}
-      </div>
-    </ul>
+    <DisplayEvent event={theEvent} key={theEvent.id} />
   );
+
   return (
     <div>
       {mappedEvents}
