8d2d2455d47ae4627f9dfcb0b45e80294de0d6ca
Jason Turner <jasonalmaturner@gmail.com>
2016-02-24 21:26:08 -0700

    more stuff

--- app/components/SendEmail/SendEmail.js
+++ app/components/SendEmail/SendEmail.js
@@ -1,9 +1,34 @@
 import React, { PropTypes } from 'react';
 import styles from './SendEmail.css';
 
-const SendEmail = () => {
+const SendEmail = ({ handleClick, sendEmail }) => {
+  let message;
+  let fromName;
+  let fromEmail;
   return (
-    <div></div>
+    <div>
+      <form onSubmit={e => {
+        handleClick(e);
+        sendEmail(message.value, fromName.value, fromEmail.value);
+        message.value = '';
+        fromName.value = '';
+        fromEmail.value = '';
+      }}>
+        <input type='email' ref={node => {
+
+          fromEmail = node;
+        }} placeholder='Enter your email address'/>
+        <input type='text' ref={node => {
+
+          fromName = node;
+        }} placeholder='Enter your name'/>
+        <textarea type='text' ref={node => {
+
+          message = node;
+        }} placeholder='Enter your message' />
+        <input type='submit' value='Send' />
+      </form>
+    </div>
   );
 };
 
