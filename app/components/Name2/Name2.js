e31062ffc2b17468cdc051ef13d79d2c6b9b6301
Jason Turner <jasonalmaturner@gmail.com>
2016-03-01 15:49:50 -0700

    don't need to upload icons

--- app/components/Name2/Name2.js
+++ app/components/Name2/Name2.js
@@ -12,9 +12,6 @@ const Name = () => (
       bgWidth='auto'>
         <div className={`${styles.background} ${styles.background2}`}>
           This is a test
-          <div className={styles.links}>
-            <img src='images/facebook.png' className={styles.icons}/>
-          </div>
         </div>
     </Parallax>
   </div>
